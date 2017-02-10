ee8deee38e304c76d69086ff1cc1e7a35065794f
mmadondo <madondomalvern@gmail.com>
2017-02-10 11:37:27 -0600

    Modified and added Spinner component

--- app/src/main/java/css/cis3334/pizzaorder/MainActivity.java
+++ app/src/main/java/css/cis3334/pizzaorder/MainActivity.java
@@ -3,10 +3,15 @@ package css.cis3334.pizzaorder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements updateViewInterface {
 
@@ -35,6 +40,29 @@ public class MainActivity extends AppCompatActivity implements updateViewInterfa
         txtStatus = (TextView) findViewById(R.id.textViewStatus);
 
         pizzaOrderSystem = new PizzaOrder(this);
+
+        // Spinner element
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+        // Spinner click listener
+        spinner.setOnItemSelectedListener(this);
+
+        // Spinner Drop down elements
+        List<String> toppings = new ArrayList<String>();
+        toppings.add("Pepperoni");
+        toppings.add("Italian Sausage");
+        toppings.add("Hawaiian");
+        toppings.add("Chicken");
+
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, toppings);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
     }
 
     @Override
@@ -44,8 +72,9 @@ public class MainActivity extends AppCompatActivity implements updateViewInterfa
 
     public void onClickOrder(View view) {
         String orderDescription = pizzaOrderSystem.OrderPizza("Pepperoni","Large", false);
+
         //display a pop up message for a long period of time
-        Toast.makeText(getApplicationContext(), "You have ordered a "+orderDescription , Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "You have ordered a "+ orderDescription , Toast.LENGTH_LONG).show();
         txtTotal.setText("Total Due: " + pizzaOrderSystem.getTotalBill().toString());
     }
 }
